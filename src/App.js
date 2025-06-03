47385328c10479672ddbe23a33dc91f8144cd6a0
Steph-duryumsu <yetbudam@gmail.com>
2025-06-03 19:24:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import Confetti from 'react-confetti';
+import { useWindowSize } from 'react-use';
 
-function App() {
+function Birthdayapp() {
+  const { width, height } = useWindowSize();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-full h-screen  flex items-center justify-center bg-gradient-to-r from-pink-300 via-purple-300 to-blue-300 bg-[length:200%_200%] animate-gradient-x"
+    // style={{backgroundImage: "url('/GIF1.gif')"}}
+    >
+
+     <confetti width={width} height={height}/>
+
+     <h1 className='text-4xl font-extrabold text-white animate-bounce' >Happy birthday 🎉</h1>
+
+     <audio>
+      <source src='' type='audio/mpeg'/>
+     </audio>
     </div>
   );
 }
 
-export default App;
+export default Birthdayapp;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      animation: {
+        'gradient-x': 'gradient-x 5s ease infinite',
+      },
+      keyframes: {
+        'gradient-x': {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
